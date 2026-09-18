9e4fb2078191a25bfa8293b25d12bc6aeced55ce
Shuo Cheng Hu <shuochenghu@gmail.com>
2026-09-18 15:27:00 +0800

    第一周課程，指令部分需要再確認

---
+++ 115_09_18/Java-01/src/HelloCourse.java
@@ -0,0 +1,8 @@
+package src;
+public class HelloCourse {
+    public static void main(String[] args) {
+        // 固定輸出，方便比較不同工具的執行結果。
+        System.out.println("Hello, Java!");
+        System.out.println("Course: Object-Oriented Programming");
+    }
+}
